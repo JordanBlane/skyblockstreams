6a39c7cc11b07f64843db692a8c7fb8fae182b86
JordanBlane <jordan26265071@live.com>
2026-05-01 08:05:18 +0100

    pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  apps: [{
+    name: 'app',
+    script: 'npm',
+    args: 'start',
+    env: {
+      NODE_ENV: 'skyblockstreams',
+      CLIENT_ID: '5es63utxphfegvmlis7odecqojjonv',
+      CLIENT_SECRET: 'o5tsy3v8mp65ke6qwkqga3p2wpmd86',
+    },
+  }],
+};
